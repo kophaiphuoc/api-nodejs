61cdbba0645594b41ee81a4611fa687e3e84b78f
kophaiphuoc <prolatui112233@gmail.com>
2022-08-08 09:17:45 +0700

    abc

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const { json } = require('body-parser');
+dotenv.config();
+const userroutter = require("./routes/user");
+const productrouter = require("./routes/products");
+const cartrouter = require("./routes/cartproduct");
+// connect to mongoose
+mongoose.connect(process.env.mongodb_url,() => {
+    console.log("connect to mongoose");
+});
+
+app.use(bodyParser.json({limit:"100mb"}));
+app.use(cors());
+app.use(morgan("common"));
+// ROUTER user
+app.use("/user",userroutter);
+// router products
+app.use("/product",productrouter);
+// router cart
+app.use("/cart",cartrouter);
+
+app.get("/api",(req,res,next)=>{
+    res.status(200).json("xin chào mọi người");
+});
+
+app.listen(9999,()=>{
+    console.log('listening on port ');
+});
